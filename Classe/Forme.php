6f7aa06806176a92c4e99e8a7448bdfe80d80d7d
Pape Samba Ndour 7777293282 <papesambandour@hotmail.com>
2017-07-26 18:41:31 +0000

    4

--- Classe/Forme.php
+++ Classe/Forme.php
@@ -6,18 +6,22 @@
  * Date: 26/07/2017
  * Time: 15:41
  */
-class Forme
+public class Forme
 {
     private $data;
     private $surround = 'p';
 
     public function __construct($data = array())
     {
-
+        echo 0;
     }
 
+    /**
+     * @param $html
+     * @return mixed
+     */
     private function surround($html)
     {
-        return "<$surround>"
+        return 0;
     }
 }
