a30b53ee6b192401193c151307257eafce28bde9
Pape Samba Ndour 7777293282 <papesambandour@hotmail.com>
2017-07-27 09:19:35 +0000

    add class user

--- Classe/Forme.php
+++ Classe/Forme.php
@@ -1,27 +1,22 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: P Samba Ndour
- * Date: 26/07/2017
- * Time: 15:41
- */
-public class Forme
+class Forme
 {
     private $data;
     private $surround = 'p';
 
     public function __construct($data = array())
     {
-        echo 0;
+
     }
 
     /**
      * @param $html
+     * @param $surround
      * @return mixed
      */
-    private function surround($html)
+    private function surround($html, $surround)
     {
-        return 0;
+        return "<{$this->surround}> {$html}</{$this->surround}>";
     }
 }
---
+++ Classe/User.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: P Samba Ndour
+ * Date: 26/07/2017
+ * Time: 18:44
+ */
+class User
+{
+    private $paco;
+    public function __construct()
+    {
+        $p
+    }
+}
